581eb08be47d85ded5da63c3b13d9d4426219ea5
MINGKY <admin@mingky.me>
2020-07-06 23:43:44 +0900

    텍스트필드 포커스시 비활성화 추가

--- content_script.js
+++ content_script.js
@@ -15,11 +15,25 @@ keyData["ㅓ"] = "/service/board/jirum";
 keyData["ㄴ"] = "/service/board/sold";
 keyData["ㅠ"] = "/service/board/hongbo";
 keyData["ㅗ"] = "/service/";
-$(document).on('keydown',function(e){
-    var pressedKey = e.key.toLowerCase();
-    $.each(keyData, function(key, val) {
-        if (key == pressedKey) {
-            window.location.href = val;
+keyData["a"] = "/service/";
+var shortCutIsVaild = false;
+setTimeout(function () {
+    shortCutIsVaild = true;
+}, 700);
+$(document).on('keydown', function (e) {
+    if (shortCutIsVaild) {
+        var tagName = e.target.nodeName;
+        if ((tagName != 'INPUT') && (tagName != 'TEXTAREA') && (tagName != 'SELECT') && (e.target.className.indexOf('fr-view') == -1) && (e.target.className.indexOf('comment-textarea') == -1)) {
+            if (e.ctrlKey) return;
+            if (e.shiftKey) return;
+            if (e.altKey) return;
+            if (e.metaKey) return;
+            var pressedKey = e.key.toLowerCase();
+            $.each(keyData, function (key, val) {
+                if (key == pressedKey) {
+                    window.location.href = val;
+                }
+            });
         }
-    });
+    }
 });
